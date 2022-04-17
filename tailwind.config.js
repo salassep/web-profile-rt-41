9f66b72f94483aeba48a8bbbf9e33c7a128db0ed
Salassep <salasanto88@gmail.com>
2022-04-17 09:51:41 +0800

    web profile rt 41 first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '56px',
+    },
+    extend: {
+      colors: {
+        primary: '#F94892',
+        lightPrimary: '#FF8AAE',
+        secondary: '#30202e',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
